dce3a1803da02492dbef386716c43a25e6fb6862
Jesus Ramirez <jesusdevelp@gmail.com>
2024-12-18 16:06:13 -0600

    Config prod

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,10 @@ async function bootstrap() {
 
 
   app.use( bodyParser.json({ limit: '10mb' }) );
-  app.use( bodyParser.urlencoded({ limit: '10mb', extended: true }) )
+  app.use( bodyParser.urlencoded({ limit: '10mb', extended: true }) );
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000
+
+  await app.listen( port );
 }
 bootstrap();
